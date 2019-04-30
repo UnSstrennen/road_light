a63ec1a926b1284a0476aba1a05cf769b5a06c0b
UnSstrennen <n-i-k-i-t@yandex.ru>
2019-04-30 12:58:58 +0300

    added switcher checker

--- main.c
+++ main.c
@@ -7,16 +7,26 @@
 #define move_sensor PB4
 #define switcher PB0
 
+const unsigned long delay_time = 1500000;
 
 int main()
 {
     // outputs
     DDRB |= (1<<relay);
+    DDRB |= (1<<switcher);
+    //inputs
+    DDRB &= ~(1 << move_sensor;
+
     while (true) {
         if (PINB & (1 << move_sensor)) {
-            PORTB |= (1<<relay);
-            _delay_ms(500);
-            PORTB &= ~(1<<relay);
-            _delay_ms(500);
+            if (!(PINB & (1 << switcher))) {
+                light();
+            }
     }
+}
+
+void light() {
+    PORTB |= (1<<relay);
+    _delay_ms(delay_time);
+    PORTB &= ~(1<<relay);
 }
