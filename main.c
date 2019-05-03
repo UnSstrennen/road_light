d8101638a968f661a78a31f3d526fb5c0a2cfcec
Un Sstrennen <n-i-k-i-t@yandex.ru>
2019-05-03 10:23:37 +0300

    working edition

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define F_CPU 1000000UL
 #define relay PB1
 #define light_sensor PB2
 #define light_regulator PB3
@@ -9,25 +10,56 @@
 
 const unsigned long delay_time = 1500000;
 
+unsigned int now_light;
+unsigned int daylight;
+
 int main()
 {
-    // outputs
-    DDRB |= 1<<relay;
-    //inputs
-    DDRB &= ~(1 << move_sensor);
-    DDRB &= ~(1 << switcher);
-    PORTB |= 1<<switcher  //  pull-up
-
-    while (true) {
-        if (PINB & (1 << move_sensor)) {
-            if (!(PINB & (1 << switcher))) {
-                light();
-            }
-    }
+	// outputs
+	DDRB |= 1<<relay;
+	//inputs
+	DDRB &= ~(1 << move_sensor);
+	DDRB &= ~(1 << switcher);
+	// PORTB |= (1 << switcher);  //  pull-up
+	
+	// ADC
+	ADCSRA |= (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);// Разрешение использования АЦП + Делитель 128 = 64 кГц
+	//ADMUX |= (1<<REFS1)|(1<<REFS0);
+	
+	while (1) {
+		if (PINB & (1 << move_sensor)) {
+			if ((PINB & (1 << switcher))) {
+				light();
+			}
+			else {
+				if (now_light < daylight) {
+					light();
+				}
+			}
+		}
+		
+		// ADC
+		now_light = getLight();
+		daylight = getDaylight();
+	}
 }
 
 void light() {
-    PORTB |= (1<<relay);
-    _delay_ms(delay_time);
-    PORTB &= ~(1<<relay);
+	PORTB |= (1<<relay);
+	_delay_ms(delay_time);
+	PORTB &= ~(1<<relay);
+}
+
+int getLight() {
+	ADMUX &= ~((1<<MUX1)|(1<<MUX0));
+	ADCSRA |= (1<<ADSC); //Начинаем преобразование
+	while((ADCSRA & (1<<ADSC))); //проверим закончилось ли аналого-цифровое преобразование
+	return (unsigned int) ADC;
+}
+
+int getDaylight() {
+	ADMUX |= (1<<MUX1)|(1<<MUX0);
+	ADCSRA |= (1<<ADSC); //Начинаем преобразование
+	while((ADCSRA & (1<<ADSC))); //проверим закончилось ли аналого-цифровое преобразование
+	return (unsigned int) ADC;
 }
