4e165e47afb4f945d461e7fdc82f52f700651fbd
Un Sstrennen <n-i-k-i-t@yandex.ru>
2019-05-03 10:51:23 +0300

    changed pins

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 #define F_CPU 1000000UL
 #define relay PB1
-#define light_sensor PB2
+#define light_sensor PB4
 #define light_regulator PB3
-#define move_sensor PB4
+#define move_sensor PB2
 #define switcher PB0
 
 const unsigned long delay_time = 1500000;
@@ -24,6 +24,7 @@ int main()
 	
 	// ADC
 	ADCSRA |= (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);// Разрешение использования АЦП + Делитель 128 = 64 кГц
+	ADMUX |= (1<<MUX1);
 	//ADMUX |= (1<<REFS1)|(1<<REFS0);
 	
 	while (1) {
@@ -51,14 +52,14 @@ void light() {
 }
 
 int getLight() {
-	ADMUX &= ~((1<<MUX1)|(1<<MUX0));
+	ADMUX &= ~(1<<MUX0);
 	ADCSRA |= (1<<ADSC); //Начинаем преобразование
 	while((ADCSRA & (1<<ADSC))); //проверим закончилось ли аналого-цифровое преобразование
 	return (unsigned int) ADC;
 }
 
 int getDaylight() {
-	ADMUX |= (1<<MUX1)|(1<<MUX0);
+	ADMUX |= (1<<MUX0);
 	ADCSRA |= (1<<ADSC); //Начинаем преобразование
 	while((ADCSRA & (1<<ADSC))); //проверим закончилось ли аналого-цифровое преобразование
 	return (unsigned int) ADC;
